c09cd3cde6864b53cc86f3794d299b74ee1efd2d
Karanei-Kimutai <karaneijunior@gmail.com>
2026-02-02 12:03:18 +0300

    added support for underline formatting

--- components/editor/TiptapEditor.tsx
+++ components/editor/TiptapEditor.tsx
@@ -5,6 +5,7 @@ import StarterKit from '@tiptap/starter-kit'
 import Image from '@tiptap/extension-image'
 import Placeholder from '@tiptap/extension-placeholder'
 import Link from '@tiptap/extension-link'
+import Underline from '@tiptap/extension-underline'
 import Toolbar from './Toolbar'
 
 type Props = {
@@ -16,6 +17,7 @@ export default function TiptapEditor({ onChange, initialContent = '' }: Props) {
   const editor = useEditor({
     extensions: [
       StarterKit,
+      Underline,
       Image.configure({
         inline: true,
         allowBase64: true,
@@ -42,6 +44,15 @@ export default function TiptapEditor({ onChange, initialContent = '' }: Props) {
 
   return (
     <div className="w-full border border-stone-200 rounded-lg overflow-hidden bg-white shadow-sm">
+      {/*
+        Ensure Toolbar supports:
+        - Bold
+        - Italic
+        - Underline
+        - Strikethrough
+        - Image
+        If not, update Toolbar.tsx accordingly.
+      */}
       <Toolbar editor={editor} />
       <div className="p-6">
         <EditorContent editor={editor} />
